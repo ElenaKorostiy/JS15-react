6c1c95ab2c844958495386f2ac62387aa1657873
Elena` <go.lena.83@gmail.com`>
2023-11-11 11:51:07 +0300

    cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,2 @@
 import React from "react";
-import "./App.css";
-import { Hero } from "./sections/hero";
-import { Main } from "./sections/main";
-import { Footer } from "./sections/footer";
 
-export const App = () => {
-  console.log();
-  return (
-      <>
-        <Hero />
-        <Main />
-        <Footer />
-      </>
-      
-  );
-};

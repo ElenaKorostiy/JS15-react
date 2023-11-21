29b3218a3ece7d69ab07e84aed2b4014e58b772d
Elena` <go.lena.83@gmail.com`>
2023-11-21 18:33:39 +0300

    dz38

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { memo, useState } from 'react';
+
+import "./App.css";
 
 export const App = () => {
-    return <>Hello</>
+    const Input = memo(({ defaultValue, onChange }) => {
+        console.log('Rendering Input component');
+
+        return (
+            <input
+                type="text"
+                defaultValue={defaultValue}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        );
+    });
+
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+        <div>
+            <Input defaultValue=" " onChange={(value) => setInputValue(value)} />
+            <p>Input value: {inputValue}</p>
+        </div>
+    );
 };

ece0f0bd9cdb96e5650941d58e09e342e7814896
Elena` <go.lena.83@gmail.com`>
2023-12-02 17:43:30 +0300

    leson42

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import { Formik, Form, Field} from "formik";
+import { Input } from "./input";
+import { InputWrap } from './input-wrap';
 
 
 import "./App.css"
@@ -6,7 +9,29 @@ import "./App.css"
 export const App = () => {
     return (
     <div>
-      
-    </div>
+  <InputWrap>
+    {(value, onChange) => <Input value={value} onChange={onChange} />}
+  </InputWrap>
+  <Formik
+    initialValues={{ firstname: "" }}
+    enableReinitialize
+    onSubmit={(values, formikBag) => {
+      console.log(values);
+      console.log(formikBag);
+    }}
+  >
+    {(props) => {
+      console.log(props);
+      return (
+        <Form>
+          <label>
+            <Field name="firstname"/>
+          </label>
+          <button type="submit">Submit</button>
+        </Form>
+      );
+    }}
+  </Formik>
+</div>
   );
 };

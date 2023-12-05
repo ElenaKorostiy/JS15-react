97566dd49358287427a8baa54588a261d9a6e10f
Elena` <go.lena.83@gmail.com`>
2023-12-05 18:20:11 +0300

    home42

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,87 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
 
+export const MyForm = () => {
+  const validate = (values) => {
+    const errors = {};
 
-import "./App.css"
+    if (!values.firstName) {
+      errors.firstName = 'First Name is required';
+    }
 
-export const App = () => {
-    return (
-    <div>
-      
-    </div>
+    if (!values.lastName) {
+      errors.lastName = 'Last Name is required';
+    }
+
+    if (!values.city) {
+      errors.city = 'City is required';
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      city: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log('Form submitted with values:', values);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstName}
+        />
+        {formik.touched.firstName && formik.errors.firstName ? (
+          <div>{formik.errors.firstName}</div>
+        ) : null}
+      </div>
+      <br/>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastName}
+        />
+        {formik.touched.lastName && formik.errors.lastName ? (
+          <div>{formik.errors.lastName}</div>
+        ) : null}
+      </div>
+      <br/>
+      <div>
+        <label htmlFor="city">City:</label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.city}
+        />
+        {formik.touched.city && formik.errors.city ? (
+          <div>{formik.errors.city}</div>
+        ) : null}
+      </div>
+      <br/>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import {MyForm } from './App'
+
 
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MyForm />
   </React.StrictMode>,
 );
 

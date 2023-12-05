ba059e1b886c3c7dcfa7e2dbc01f868e8e365b0d
Elena` <go.lena.83@gmail.com`>
2023-12-05 19:30:58 +0300

    rrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
-import React from "react";
-import "./App.css";
 import { Hero } from "./sections/hero";
 import { Main } from "./sections/main";
 import { Footer } from "./sections/footer";
-import { LanguageProvider, useLanguage } from "./language/lang";
+import { LanguageProvider } from "./language/lang";
+import { LanguageButton } from "./language/langbutton";
+
+import "./App.css";
 
 
 export const App = () => {
    return (
     <LanguageProvider>
-      <AppContent />
-    </LanguageProvider>
-  );
-};
-  
-  const AppContent = () => {
-    const { toggleLanguage } = useLanguage();
-  
-  return (
-    <>
-      <button className="lang" onClick={toggleLanguage}>Language</button>
+     <LanguageButton/>
       <Hero />
       <Main />
       <Footer />
-    </>
-      
+    </LanguageProvider>
   );
 };
+
 
--- src/language/langbutton.jsx
+++ src/language/langbutton.jsx
@@ -1,34 +1,10 @@
-// import { useLanguage } from "../../language/lang";
-
-// export const LanguageButton = () => {
-//   const { language, toggleLanguage } = useLanguage();
-//   const [buttonText, setButtonText] = useState("Language");
-
-//   useEffect(() => {
-//     setButtonText(language === "en" ? "English" : "Українська");
-//   }, [language]);
-
-//   return (
-//     <button className="lang" onClick={toggleLanguage}>
-//       {buttonText}
-//     </button>
-//   );
-// };
-
-import React, { useEffect, useState } from 'react';
-import { useLanguage } from '../../language/lang';
+import { useLanguage } from './lang';
 
 export const LanguageButton = () => {
   const { language, toggleLanguage } = useLanguage();
-  const [buttonText, setButtonText] = useState('Language');
-
-  useEffect(() => {
-    setButtonText(language === 'en' ? 'English' : 'Українська');
-  }, [language]);
-
-  return (
+   return (
     <button className="lang" onClick={toggleLanguage}>
-      {buttonText}
+      {language === "en" ? "ukr" : "en"}
     </button>
   );
 };

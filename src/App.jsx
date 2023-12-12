d78ec8ae24109d5222f377b85680e26ba17b98df
Elena` <go.lena.83@gmail.com`>
2023-12-12 22:41:42 +0300

    home43

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
-import React, { useRef } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 
 
-import "./App.css"
-
-export const App = () => {
-    return (
+export const MyForm = () => {
+  const { handleSubmit, control, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
     <div>
-      
-    </div>
+        <label>First Name</label>
+        <Controller
+          name="firstName"
+          control={control}
+          defaultValue=""
+          rules={{ required: "First name is required" }}
+          render={({ field }) => (
+            <div>
+              <input {...field} placeholder="First Name" />
+              {errors.firstName && <p>{errors.firstName.message}</p>}
+            </div>
+          )}
+        />
+      </div>
+      <br/>
+      <div>
+        <label>Last Name</label>
+        <Controller
+          name="lastName"
+          control={control}
+          defaultValue=""
+          rules={{ required: "Last name is required" }}
+          render={({ field }) => (
+            <div>
+              <input {...field} placeholder="Last Name" />
+              {errors.lastName && <p>{errors.lastName.message}</p>}
+            </div>
+          )}
+        />
+      </div>
+      <br/>
+      <div>
+        <label>City</label>
+        <Controller
+          name="city"
+          control={control}
+          defaultValue=""
+          rules={{ required: "City is required" }}
+          render={({ field }) => (
+            <div>
+              <input {...field} placeholder="City" />
+              {errors.city && <p>{errors.city.message}</p>}
+            </div>
+          )}
+        />
+      </div>
+      <br/>
+      <button type="submit">Submit</button>
+    </form>
   );
 };

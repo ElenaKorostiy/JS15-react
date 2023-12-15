c5eb274c928f588d6f9cdafd74c7d1c100ebe059
Elena` <go.lena.83@gmail.com`>
2023-12-15 15:45:29 +0300

    leson44

---
+++ src/components/user.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+
+export const User = () => {
+  const params = useParams();
+  console.log("prams==>>", params);
+  const name = `${params.id}`.toUpperCase();
+  return <p>{name}</p>;
+};
